cd09b5e7e911e7897cab0b034030eecdea91f371
Alex Berry <alexander.e.berry@hotmail.co.uk>
2017-07-22 14:20:01 +0100

    create new project folder containing react and rails

---
+++ MoneyManager/react/client/src/app.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+window.addEventListener('load', function () {
+  ReactDOM.render(
+    <h1>App Started</h1>,
+    document.getElementById('app')
+  );
+});
---
+++ MoneyManager/react/webpack.config.js
@@ -0,0 +1,25 @@
+const config = {
+  entry: `${__dirname}/client/src/app.js`,
+  output: {
+    filename: "bundle.js",
+    path: `${__dirname}/client/build`
+  },
+  devtool: "source-map",
+  resolve: {
+    extensions: ['.js', '.jsx']
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        options: {
+          presets: ['react']
+        }
+      }
+    ]
+  }
+};
+
+module.exports = config;
