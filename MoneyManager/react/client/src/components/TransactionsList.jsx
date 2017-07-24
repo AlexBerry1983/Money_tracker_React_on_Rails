cc8b7ca448f47421eb8cbe281c4612e4738ac446
Alex Berry <alexander.e.berry@hotmail.co.uk>
2017-07-24 11:56:45 +0100

    add transaction total to transaction page

--- MoneyManager/react/client/src/components/TransactionsList.jsx
+++ MoneyManager/react/client/src/components/TransactionsList.jsx
@@ -3,14 +3,29 @@ import TransactionItem from './TransactionItem'
 
 class TransactionsList extends React.Component{
 
+  constructor(props){
+    super(props)
+  }
+
+  calculateTotal(array){
+    let totalSpent = 0
+    array.forEach(function(object){
+      totalSpent += Number(object.amount)
+    })
+    console.log(totalSpent);
+    return totalSpent
+  }
+
   render(){
-    let transactionArray = this.props.transactionInfo.map((transaction, index) => {
+    const transactionArray = this.props.transactionInfo.map((transaction, index) => {
       return <TransactionItem key={index} name={transaction.name} amount={transaction.amount}/>
     })
+
 
     return(
       <div>
         {transactionArray}
+        Total spent: {this.calculateTotal(this.props.transactionInfo)}
       </div>
     )
   }
