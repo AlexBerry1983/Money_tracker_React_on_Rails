161cbdbdadab4bc358132096ccf31335cb435cbf
Alex Berry <alexander.e.berry@hotmail.co.uk>
2017-07-24 20:13:35 +0100

    add home button to transaction list

--- MoneyManager/react/client/src/components/TransactionsList.jsx
+++ MoneyManager/react/client/src/components/TransactionsList.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import TransactionItem from './TransactionItem';
-import ReactDOM from 'react-dom';
+import {Link} from 'react-router-dom';
 
 class TransactionsList extends React.Component{
 
